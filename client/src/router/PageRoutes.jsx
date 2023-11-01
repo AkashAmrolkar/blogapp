6e0d2d8e848a0910bce08af964b95c51bd8246ea
akash <akashamrolkar10@gmail.com>
2023-11-01 23:08:21 +0530

    home page

--- client/src/router/PageRoutes.jsx
+++ client/src/router/PageRoutes.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import Register from "../pages/Register"
 import Login from "../pages/Login"
+import Home from "../pages/Home"
 
 const PageRoutes = () => {
   return (
     <Routes>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/register" element = {<Register />} />
         <Route path="/login" element = {<Login />} />
     </Routes>
