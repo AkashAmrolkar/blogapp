fa31f45feae3888448a3727e8d911a1d31085b8f
akash <akashamrolkar10@gmail.com>
2023-11-01 18:13:16 +0530

    header

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <nav className="bg-blue-500 p-4">
+        <div className="container mx-auto flex justify-between items-center">
+        <Link to={'/'} className="text-white hover:text-blue-300 text-2xl font-bold">Your Logo</Link>
+          <div className="space-x-4">
+            <Link to={'/#'} className="text-white font-semibold hover:text-blue-300">Home</Link>
+            <Link to={'/#'} className="text-white font-semibold hover:text-blue-300">About</Link>
+            <Link to={'/#'} className="text-white font-semibold hover:text-blue-300">Contact</Link>
+            <Link to={'/#'} className="text-white font-semibold hover:text-blue-300">Login</Link>
+          </div>
+        </div>
+      </nav>  
+    </div>
   )
 }
 
