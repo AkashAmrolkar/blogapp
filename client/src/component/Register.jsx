99054221e8a4e2e743431a08dd3935c62f6bedd5
akash <akashamrolkar10@gmail.com>
2023-11-01 11:46:25 +0530

    Register final

--- client/src/component/Register.jsx
+++ client/src/component/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 const Register = () => {
 
@@ -15,9 +15,10 @@ const Register = () => {
             [e.target.name]: e.target.value
         })
     }
-    const handleSubmit = async()=>{
+    const handleSubmit = async(e)=>{
+        e.preventDefault();
         try {
-          const response = await fetch('/api/users/register', {
+          const response = await fetch('http://localhost:8000/api/users/register', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
@@ -40,26 +41,28 @@ const Register = () => {
     <div className="container mx-auto p-4">
     <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
       <div className="mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="firstname">
           First Name
         </label>
         <input
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="text"
           name="firstname"
+          id='firstname'
           value={formData.firstname}
           onChange={handleChange}
           required
         />
       </div>
       <div className="mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastname">
           Last Name: 
         </label>
         <input
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="text"
           name="lastname"
+          id="lastname"
           value={formData.lastname}
           onChange={handleChange}
           required
@@ -73,6 +76,7 @@ const Register = () => {
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="email"
           name="email"
+          id='email'
           value={formData.email}
           onChange={handleChange}
           required
@@ -86,6 +90,7 @@ const Register = () => {
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="password"
           name="password"
+          id='password'
           value={formData.password}
           onChange={handleChange}
           required
