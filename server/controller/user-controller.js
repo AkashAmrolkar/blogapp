0de6bff2d7963531ec61079ceb81454bf131bfa1
akash <akashamrolkar10@gmail.com>
2023-11-06 23:43:36 +0530

    test

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -79,6 +79,22 @@ export const login = async (req, res, next) => {
         console.log(error)
     }
 
-
-
 }
+
+const verify = (req, res, next)=>{
+    const authHeader = req.header.authorization;
+    if(authHeader){
+        const token = authHeader.split(" ")[1];
+        jwt.verify(token, 'mySecretKey', (err, user)=>{
+            if(err){
+                return res.status(403).json('Token is not valid')
+            }
+
+            req.user = user
+            next()
+        })
+    } else{
+        res.status(401).json("you are not authenticated")
+    }
+}
+
