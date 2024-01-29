d732ef585e47afd4e87cae08041c231a923ae260
AkashAmrolkar <akashamrolkar10@gmail.com>
2024-01-29 23:46:41 +0530

    added categories

---
+++ client/src/categories.js
@@ -0,0 +1,28 @@
+export const categories = [
+    {
+        slug: 'techology',
+        name: 'Techology',
+        image: ''
+    },
+    {
+        slug: 'travel',
+        name: 'Travel',
+        image: ''
+    },
+    {
+        slug: 'food',
+        name: 'Food',
+        image: ''
+    },
+    {
+        slug: 'sports',
+        name: 'Sports',
+        image: ''
+    },
+    {
+        slug: '',
+        name: '',
+        image: ''
+    },
+    
+]
