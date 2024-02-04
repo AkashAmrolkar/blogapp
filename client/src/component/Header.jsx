c6449187115144f6223d93a833e62da014a9356e
AkashAmrolkar <akashamrolkar10@gmail.com>
2024-02-04 23:28:16 +0530

    header Ui

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,19 +1,27 @@
 import { Link } from "react-router-dom"
 import { useAuth } from "../store/Auth"
-import { IoMenu, IoClose  } from "react-icons/io5";
+import { IoMenu, IoClose } from "react-icons/io5";
+import { FaUser  } from "react-icons/fa";
 import { useState } from "react";
 
 const Header = () => {
-  const {isLoggedIn} = useAuth();
+  const {isLoggedIn, userData} = useAuth();
   //console.log("Is LoggedIn", isLoggedIn)
 
   const [openMenu, setOpenMenu] = useState(true);
+  const [profile, setProfile] = useState(false);
 
   const handleMenu = () =>{
     setOpenMenu(!handleMenu);
   }
   const handleCloseMenu = () =>{
     setOpenMenu(true);
+  }
+  const handleHover = () =>{
+    setProfile(!profile)
+  }
+  const removeProfile = () =>{
+    setProfile(!profile)
   }
   return (
     <div className=" bg-transparent shadow-md p-4 fixed top-0 w-full bg-white z-10">
@@ -32,16 +40,26 @@ const Header = () => {
               !isLoggedIn && <><Link to={'/register'} className="nav-link text-[#302D55] font-semibold">Register</Link><Link to={'/login'} className="nav-link text-white bg-gradient-to-r from-[#FC6668] to-[#E10489] font-semibold px-6 py-2 rounded-xl hover:opacity-80">Login</Link></>
             }
           </div>
-          <div className="flex justify-between items-center md:hidden ">
+          <div className="flex justify-between items-center md:hidden gap-4 ">
+            <FaUser className="border rounded-full h-4 w-4 text-5xl" onClick={handleHover} />
             {
-              isLoggedIn && 'Profile'
+              profile &&
+              <div className="flex flex-col gap-4 bg-white shadow-md absolute top-20 w-fit right-0 transition-all delay-300 ease-in-out p-5 z-10" onClick={removeProfile}>
+                {
+                  isLoggedIn && <><Link to={`/profile/${userData?._id}`} className="nav-link text-[#302D55] font-semibold">View Profile</Link> <Link to={'/logout'} className="nav-link text-white bg-gradient-to-r from-[#FC6668] to-[#E10489] font-semibold px-6 py-2 rounded-xl hover:opacity-80 w-fit">Logout</Link></>
+
+                }
+                {
+                  !isLoggedIn && <><Link to={'/register'} className="nav-link text-[#302D55] font-semibold w-fit">Register</Link><Link to={'/login'} className="nav-link text-white bg-gradient-to-r from-[#FC6668] to-[#E10489] font-semibold px-6 py-2 rounded-xl hover:opacity-80 w-fit">Login</Link></>
+                }
+              </div>
             }
             {
               openMenu ? <IoMenu className="text-3xl" onClick={handleMenu} /> : <IoClose className={`text-3xl `}  onClick={handleCloseMenu} />
             }
             {
               !openMenu &&
-              <div className="flex flex-col gap-4 bg-white shadow-md absolute top-16 w-1/2 transition-all delay-300 -translate-x-full ease-in-out right-0 p-5 z-10">
+              <div className="flex flex-col gap-4 bg-white shadow-md absolute top-16 w-full transition-all delay-500 translate-x-0 ease-in-out right-0 p-5 z-10">
                 <Link to={'/#'} className="nav-link text-[#302D55] font-semibold hover:opacity-80">Home</Link>
                 <Link to={'/posts'} className="nav-link text-[#302D55] font-semibold hover:opacity-80">Posts</Link>
                 <Link to={'/contact'} className="nav-link text-[#302D55] font-semibold hover:opacity-80">Contact</Link>
