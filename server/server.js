a38b544e9c2fc31a3527a0d5a203c26f02c9e20c
AkashAmrolkar <akashamrolkar10@gmail.com>
2024-02-10 02:26:17 +0530

    deployment

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.json({limit: '50mb'}));
 
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173',
+    origin: 'https://blogapp-akash-amrolkar.vercel.app',
 }));
 app.use('/api/users', userRoute)
 app.use('/api/blogs', blogRouter)
