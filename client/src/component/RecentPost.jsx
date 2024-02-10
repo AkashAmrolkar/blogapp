149f1c14bbf67a925bf5bc531fbb8ae0140e4c9a
AkashAmrolkar <akashamrolkar10@gmail.com>
2024-02-10 13:23:19 +0530

    recent posts

--- client/src/component/RecentPost.jsx
+++ client/src/component/RecentPost.jsx
@@ -19,17 +19,35 @@ const RecentPost = () => {
     //const recentPosts = posts?.blogs.slice(0, 3);
 
     //console.log(recentPosts)
-    if(loading) return <Facebook/>
   return (
-    <div className=' px-5 py-8 rounded-xl bg-[#fff9f3] mb-6'>
-        
-        <h2 className=' text-black font-bold text-2xl mb-5'>Recent Posts</h2>
+
+    <>
         {
-            posts?.map((post)=>(
-                <PopularArticles post={post} key={post._id} title="Recent Posts" />
-            ))
+            loading ? <ContentLoader
+            viewBox="0 0 400 200"
+            width={400}
+            height={200}
+            title="Loading news..."
+            {...props}
+          >
+            <rect x="42.84" y="9.93" rx="5" ry="5" width="143.55" height="86.59" />
+            <rect x="192.84" y="9.67" rx="0" ry="0" width="148.72" height="100" />
+        
+            <rect x="42.84" y="107" rx="5" ry="5" width="143.55" height="86.59" />
+            <rect x="192.84" y="107" rx="0" ry="0" width="148.72" height="100" />
+          </ContentLoader> : 
+          <div className=' px-5 py-8 rounded-xl bg-[#fff9f3] mb-6'>
+        
+          <h2 className=' text-black font-bold text-2xl mb-5'>Recent Posts</h2>
+          {
+              posts?.map((post)=>(
+                  <PopularArticles post={post} key={post._id} title="Recent Posts" />
+              ))
+          }
+      </div>
         }
-    </div>
+    </>
+    
   )
 }
 
