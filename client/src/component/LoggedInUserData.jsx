e452d218d079adaffa4cd8c2abc3f5f4c0e0eb93
AkashAmrolkar <akashamrolkar10@gmail.com>
2024-02-10 13:39:16 +0530

    revert

--- client/src/component/LoggedInUserData.jsx
+++ client/src/component/LoggedInUserData.jsx
@@ -2,14 +2,12 @@ import React, {useEffect, useState} from 'react'
 import { NavLink } from 'react-router-dom'
 import { FaXTwitter, FaFacebook, FaInstagram } from "react-icons/fa6";
 import {useAuth} from '../store/Auth'
-import ContentLoader from "react-content-loader"
 
 
-const LoggedInUserData = ({author, title}, props) => {
+const LoggedInUserData = ({author, title}) => {
   const {token} = useAuth();
 
   const [userData, setUserData] = useState(null);
-  const [loading, setLoading] = useState(true)
   
   useEffect(()=>{
     const fetchData = async()=>{
@@ -21,7 +19,6 @@ const LoggedInUserData = ({author, title}, props) => {
         },
       }).then(response => response.json()).then(data => {
         setUserData(data);
-        setLoading(false)
       }).catch(error => console.error('Error fetching user data:', error));
     }
     fetchData()
@@ -32,25 +29,7 @@ const LoggedInUserData = ({author, title}, props) => {
     return data.substring(0, maxLength)
   }
   return (
-    <>
-      {
-        loading ?  <ContentLoader 
-        speed={2}
-        width={476}
-        height={124}
-        viewBox="0 0 476 124"
-        backgroundColor="#f3f3f3"
-        foregroundColor="#ecebeb"
-        {...props}
-      >
-        <rect x="48" y="8" rx="3" ry="3" width="88" height="6" /> 
-        <rect x="48" y="26" rx="3" ry="3" width="52" height="6" /> 
-        <rect x="0" y="56" rx="3" ry="3" width="410" height="6" /> 
-        <rect x="0" y="72" rx="3" ry="3" width="380" height="6" /> 
-        <rect x="0" y="88" rx="3" ry="3" width="178" height="6" /> 
-        <circle cx="20" cy="20" r="20" />
-      </ContentLoader> : 
-      <div className=' p-5 rounded-xl bg-[#fff9f3] mb-6'>
+    <div className=' p-5 rounded-xl bg-[#fff9f3] mb-6'>
         <h2 className=' text-black font-bold text-2xl mb-5'>{title}</h2>
         <div className=' flex gap-5 items-center mb-5'>
             <div>
@@ -78,11 +57,9 @@ const LoggedInUserData = ({author, title}, props) => {
           
         </div>
         <NavLink to={`/profile/${userData?._id}`} className='text-white font-semibold bg-gradient-to-r from-[#FC6668] to-[#E10489] px-6 py-2 rounded-xl hover:opacity-80 mt-6 block w-fit'>View Profile</NavLink>
-      
-      </div>
-      }
-    </>
-    
+       
+        
+    </div>
   )
 }
 
--- client/src/component/RecentPost.jsx
+++ client/src/component/RecentPost.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import PopularArticles from './PopularArticles'
-import { ContentLoader} from 'react-content-loader'
+import { Facebook} from 'react-content-loader'
 
-const RecentPost = (props) => {
+const RecentPost = () => {
     const [posts, setPosts] = useState(null)
-    const [loading, setLoading] = useState(true);
+    //const [loading, setLoading] = useState(true);
     const limit=3
     useEffect(()=>{
         fetch(`https://blogapp-backend-ten.vercel.app/api/blogs?limit=${limit}`,{
@@ -13,41 +13,23 @@ const RecentPost = (props) => {
             return res.json()
         }).then((data)=>{
             setPosts(data['blogs'])
-            setLoading(false)
+            //setLoading(false)
         })
     }, [])
     //const recentPosts = posts?.blogs.slice(0, 3);
 
     //console.log(recentPosts)
+    if(loading) return <Facebook/>
   return (
-
-    <>
+    <div className=' px-5 py-8 rounded-xl bg-[#fff9f3] mb-6'>
+        
+        <h2 className=' text-black font-bold text-2xl mb-5'>Recent Posts</h2>
         {
-            loading ? <ContentLoader
-            viewBox="0 0 400 200"
-            width={400}
-            height={200}
-            title="Loading news..."
-            {...props}
-          >
-            <rect x="42.84" y="9.93" rx="5" ry="5" width="143.55" height="86.59" />
-            <rect x="192.84" y="9.67" rx="0" ry="0" width="148.72" height="100" />
-        
-            <rect x="42.84" y="107" rx="5" ry="5" width="143.55" height="86.59" />
-            <rect x="192.84" y="107" rx="0" ry="0" width="148.72" height="100" />
-          </ContentLoader> : 
-          <div className=' px-5 py-8 rounded-xl bg-[#fff9f3] mb-6'>
-        
-          <h2 className=' text-black font-bold text-2xl mb-5'>Recent Posts</h2>
-          {
-              posts?.map((post)=>(
-                  <PopularArticles post={post} key={post._id} title="Recent Posts" />
-              ))
-          }
-      </div>
+            posts?.map((post)=>(
+                <PopularArticles post={post} key={post._id} title="Recent Posts" />
+            ))
         }
-    </>
-    
+    </div>
   )
 }
 
