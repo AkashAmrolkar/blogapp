dbc7c75b329c15265899d0105d2de003723ad9c1
AkashAmrolkar <akashamrolkar10@gmail.com>
2024-02-10 12:41:54 +0530

    header changes for mobile view

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -12,12 +12,15 @@ const Header = () => {
   const [profile, setProfile] = useState(false);
   const handleMenu = () => {
     setOpenMenu(!handleMenu);
+    setProfile(false)
   }
   const handleCloseMenu = () => {
     setOpenMenu(true);
+    setProfile(false)
   }
   const handleHover = () => {
     setProfile(!profile)
+    setOpenMenu(true)
   }
   const removeProfile = () => {
     setProfile(!profile)
@@ -41,12 +44,15 @@ const Header = () => {
                 !isLoggedIn && <><Link to={'/register'} className="nav-link text-[#302D55] font-semibold">Register</Link><Link to={'/login'} className="nav-link text-white bg-gradient-to-r from-[#FC6668] to-[#E10489] font-semibold px-6 py-2 rounded-xl hover:opacity-80">Login</Link></>
               }
             </div>
+
+            //Mobile View
+
             <div className="flex justify-between items-center md:hidden gap-4 ">
 
               <FaUser className="border rounded-full h-4 w-4 text-5xl" onClick={handleHover} />
               {
                 profile &&
-                <div className="flex flex-col gap-4 bg-white shadow-md absolute top-20 w-fit right-0 transition-all delay-300 ease-in-out p-5 z-10" onClick={removeProfile}>
+                <div className="flex flex-col gap-4 bg-white shadow-md absolute top-[55px] w-fit right-0 transition-all delay-300 ease-in-out p-5 z-10" onClick={removeProfile}>
                   {
                     isLoggedIn && <><Link to={`/profile/${userData?._id}`} className="nav-link text-[#302D55] font-semibold">View Profile</Link> <Link to={'/logout'} className="nav-link text-white bg-gradient-to-r from-[#FC6668] to-[#E10489] font-semibold px-6 py-2 rounded-xl hover:opacity-80 w-fit">Logout</Link></>
 
@@ -61,17 +67,10 @@ const Header = () => {
               }
               {
                 !openMenu &&
-                <div className="flex flex-col gap-4 bg-white shadow-md absolute top-16 w-full transition-all delay-500 translate-x-0 ease-in-out right-0 p-5 z-10">
+                <div className="flex flex-col gap-4 bg-white shadow-md absolute top-[55px] w-full transition-all delay-500 translate-x-0 ease-in-out right-0 p-5 z-10">
                   <Link to={'/#'} className="nav-link text-[#302D55] font-semibold hover:opacity-80">Home</Link>
                   <Link to={'/posts'} className="nav-link text-[#302D55] font-semibold hover:opacity-80">Posts</Link>
                   <Link to={'/contact'} className="nav-link text-[#302D55] font-semibold hover:opacity-80">Contact</Link>
-                  {
-                    isLoggedIn && <><Link to={'/create-post'} className="nav-link text-[#302D55] font-semibold">Create Post</Link> <Link to={'/logout'} className="nav-link text-white bg-gradient-to-r from-[#FC6668] to-[#E10489] font-semibold px-6 py-2 rounded-xl hover:opacity-80 w-fit">Logout</Link></>
-
-                  }
-                  {
-                    !isLoggedIn && <><Link to={'/register'} className="nav-link text-[#302D55] font-semibold w-fit">Register</Link><Link to={'/login'} className="nav-link text-white bg-gradient-to-r from-[#FC6668] to-[#E10489] font-semibold px-6 py-2 rounded-xl hover:opacity-80 w-fit">Login</Link></>
-                  }
                 </div>
               }
             </div>
